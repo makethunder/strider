05458af9651ce290e138963f2ac99175fb1f380c
Vasili Zolotov <vasili.zolotov@paperg.com>
2014-09-16 02:14:59 +0000

    fix regression in redirect logic

--- routes/index.js
+++ routes/index.js
@@ -29,7 +29,7 @@ var TEST_AND_DEPLOY = "TEST_AND_DEPLOY";
 exports.index = function(req, res){
   if (req.loggedIn === false){
   }
-  if (req.session && req.session.return_to !== undefined) {
+  if (req.session && req.session.return_to) {
     var return_to = req.session.return_to;
     req.session.return_to=null;
     res.redirect(return_to);
